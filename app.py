7f9207970ea3c5aec57331a610d22565b3d24d7a
MAHAICHI30 <aichimah03@gmail.com>
2026-06-26 00:53:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 from ultralytics import YOLO
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__) 
 CORS(app)
 
 # =======================================================
